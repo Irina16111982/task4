29d6a8bfd27d914cfe218dc9cc68ce0882dd4c48
Irina16111982 <i.a.kaparushkina@tmn3.etagi.com>
2024-10-29 08:43:30 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print('4ht program')
+print(type('123.456'))
+print(type(int(123.456)))  # преобразование строки в число
+print(123.456 * 10)  # получила число 1234,56
+a = 123.456 * 10
+b = a % 10
+print(b)
+print(int(b))
